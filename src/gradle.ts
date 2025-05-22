f185ecbf601a4b3effefa600449fd3943e2deffd
matt.bayley <matt.bayley@airbyte.io>
2025-05-22 15:55:49 -0700

    gradlew in subdirectory

--- src/gradle.ts
+++ src/gradle.ts
@@ -1,11 +1,13 @@
 import * as exec from '@actions/exec'
+import path from 'path'
 
 export async function generateDependenciesFiles(
+  subdirectory: string,
   configuration: string,
   cwd?: string
 ): Promise<void> {
   await exec.getExecOutput(
-    './gradlew',
+    path.join(subdirectory, 'gradlew'),
     ['clean', 'dependencyReport', '--configuration', configuration],
     {
       cwd: cwd
